c0d6e72dfd3ff8bfff0a1fddbb868744abd9e2a4
David-mwendwa <klatzdavie19@gmail.com>
2022-02-16 12:32:14 +0300

    do basic routing & connect middlewares"

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors')
 
 // express
 const express = require('express');
@@ -6,6 +7,19 @@ const app = express();
 
 // database
 const connectDB = require('./db/connect');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('e commerce api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 // start server
 const port = process.env.PORT || 5000;
