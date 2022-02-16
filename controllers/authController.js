ff33d06f633531c181ddb710c2b8a404c74b525d
David-mwendwa <klatzdavie19@gmail.com>
2022-02-16 14:55:45 +0300

    set up jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
 const CustomError = require('../errors');
 
 const register = async (req, res) => {
@@ -13,7 +14,12 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = { user: user.name, userId: user._id, role: user.role };
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 
 const login = async (req, res) => {
