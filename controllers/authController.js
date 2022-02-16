2d5a66e68d42f94e29a58ce6a39322bcb8a70ee3
David-mwendwa <klatzdavie19@gmail.com>
2022-02-16 14:19:17 +0300

    validate email and handle user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,19 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const register = async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
