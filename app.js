51ec365178371bea8fb04fd2bbd24f1fbcdea723
David-mwendwa <klatzdavie19@gmail.com>
2022-02-16 12:08:40 +0300

    start express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('E-Commerce API');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//start server
+(async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+})();
