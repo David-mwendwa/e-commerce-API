7d0ef020bbd33856911de90cd66b289797829b73
David-mwendwa <klatzdavie19@gmail.com>
2022-02-18 15:19:08 +0300

    complete authorize permissions

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -17,13 +17,15 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-const authorizePermissions = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    throw new CustomError.UnauthorizedError(
-      'Unauthorized to access this route'
-    );
-  }
-  next();
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next()
+  };
 };
 
 module.exports = {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,7 @@ const {
   updateUserPassword,
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, authorizePermissions, getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
 router.route('/showMe').post(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
