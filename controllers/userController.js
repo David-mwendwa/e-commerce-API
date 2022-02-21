0fe330ac4b3059583e1ec33c612f41fe1ef6a3bc
David-mwendwa <klatzdavie19@gmail.com>
2022-02-21 08:44:20 +0300

    create utility function to check permissions - for getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { createTokenUser, attachCookiesToResponse } = require('../utils');
+const {
+  createTokenUser,
+  attachCookiesToResponse,
+  checkPermissions,
+} = require('../utils');
 
 const getAllUsers = async (req, res) => {
   const users = await User.find({ role: 'user' }).select('-password');
@@ -15,6 +19,7 @@ const getSingleUser = async (req, res) => {
   if (!user) {
     throw new CustomError.NotFoundError(`No user with id: ${userId}`);
   }
+  checkPermissions(req.user, user._id);
   res.status(StatusCodes.OK).json({ user });
 };
 
