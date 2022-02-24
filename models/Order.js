a2301a526f85bdae7ca56e1341a2cd16175caa4e
David-mwendwa <klatzdavie19@gmail.com>
2022-02-24 18:31:45 +0300

    create order schema

---
+++ models/Order.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const SingleCartItemSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    orderItems: {
+      type: [SingleCartItemSchema],
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'cancelled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
